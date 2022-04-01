db49332272de67db0805e99ffe4bb777f225c935
sunkilmoons <bcassidy50@gmail.com>
2022-04-01 12:16:59 -0500

    print results in correct format

--- src/Main.java
+++ src/Main.java
@@ -9,50 +9,30 @@ public class Main {
     public static void main(String[] args) {
 //        String fileName = args[0];
 
-        String fileName = "./data/random.csv";
-//       String fileName = "./data/WhataburgerData.csv";
+//        String fileName = "./data/random.csv";
+       String fileName = "./data/WhataburgerData.csv";
 //        String fileName = "./data/StarbucksData.csv";
 
         // mock query for testing
-        Query query = new Query(3, 29.5827351, -98.621094);
+//        Query query = new Query(3, 29.5827351, -98.621094);
 
-        System.out.printf("Searching for the %d closest store...\n", query.getNumber());
 
-        List<Store> stores = getStores(fileName, query);
+        String queriesFile = "./data/Queries.csv";
+        List<Query> queries = getQueries(queriesFile);
 
-//        fileName = "./data/Queries.csv";
-//        List<Query> queries = getQueries(fileName);
-
-//        System.out.printf("Before partitioning stores are: %s\n", Store.listIdsWithDistance(stores));
-
-        Store unoptimized = getNthClosestStoreUnoptimized(stores, query.getNumber() - 1);
-        Store nthClosestStore = selectNthClosestStore(stores, 0, stores.size() - 1, query.getNumber() - 1);
-
-//        System.out.printf("After partitioning stores are: %s\n", Store.listIdsWithDistance(stores));
-
-        /**
-         * Test the function against the unoptimized one to ensure it's working correctly
-         */
-        if (nthClosestStore.getDistanceFromQuery() != unoptimized.getDistanceFromQuery()) {
-            System.out.printf("Stores are not equal...\nUnoptimized: %s\nYours: %s\n", unoptimized, nthClosestStore);
+        for (Query query : queries) {
+            List<Store> stores = getStores(fileName);
+            stores.forEach((s) -> s.computeDistanceFromQuery(query.getQlat(), query.getQlong()));
+            selectNthClosestStore(stores, 0, stores.size() - 1, query.getNumber() - 1);
+            ArrayList<Store> newStores = new ArrayList<>();
+            int i = 0;
+            while (i < query.getNumber()) {
+                newStores.add(stores.get(i));
+                i++;
+            }
+            insertionSort(newStores);
+            printQueryResults(newStores, query);
         }
-        else {
-            System.out.println("Good job, the stores are equal");
-        }
-
-//        System.out.println("Performing insertion sort...");
-//        // drop unused data
-//        ArrayList<Store> newStores = new ArrayList<>();
-//        int i = 0;
-//        while (i < query.getNumber()) {
-//            newStores.add(stores.get(i));
-//            i++;
-//        }
-//        System.out.printf("Before insertion sort: %s\n", Store.listIdsWithDistance(newStores));
-//        insertionSort(stores);
-//        System.out.printf("After insertion sort %s\n", Store.listIdsWithDistance(newStores));
-
-//        printQueryResults(stores, query);
     }
 
     public static void insertionSort(List<Store> stores)
@@ -97,21 +77,21 @@ public class Main {
      */
     private static Store selectNthClosestStore(List<Store> stores, int l, int r, int i) {
         if (l >= r) {
-            System.out.printf("l (%d) is greater than or equal to r (%d)\n", l, r);
+//            System.out.printf("l (%d) is greater than or equal to r (%d)\n", l, r);
             return l > r ? stores.get(r) : stores.get(l);
         }
         int z = randomPartition(stores, l, r);
         int k = z - l;
-        System.out.printf("i = %d, l = %d, r = %d, z = %d, k = %d\n", i, l, r, z, k);
+//        System.out.printf("i = %d, l = %d, r = %d, z = %d, k = %d\n", i, l, r, z, k);
         if (i == k) {
             return stores.get(z);
         }
         else if (i < k) {
-            System.out.println("i < k");
+//            System.out.println("i < k");
             return selectNthClosestStore(stores, l, z - 1, i);
         }
         else {
-            System.out.println("i > k");
+//            System.out.println("i > k");
             return selectNthClosestStore(stores, z + 1, r, i - k);   
         }
     }
@@ -150,9 +130,10 @@ public class Main {
     private static void printQueryResults(List<Store> stores, Query query) {
         System.out.printf("The %d closest stores to (%f, %f):\n", stores.size(), query.getQlat(), query.getQlong());
         System.out.println(stores.stream().map(Store::toString).collect(Collectors.joining("\n")));
+        System.out.println("");
     }
 
-    private static List<Store> getStores(String fileName, Query query) {
+    private static List<Store> getStores(String fileName) {
         ArrayList<Store> stores = new ArrayList<>();
         try {
             File file = new File(fileName);
@@ -172,7 +153,6 @@ public class Main {
 
                 try {
                     Store store = Store.fromTokens(tokens);
-                    store.computeDistanceFromQuery(query.getQlat(), query.getQlong());
                     stores.add(store);
                 } catch (Exception e) {
                     e.printStackTrace();
